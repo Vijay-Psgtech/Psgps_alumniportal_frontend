dbc2c2c5ef91de732ab9b6df36dc337f3799242c
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-09-24 11:38:33 +0530

    Noteable Alumni Data json content file has been added.

---
+++ src/content/data/NoteableAlumniData.js
@@ -0,0 +1,164 @@
+import alumniImg1 from "../../assets/Alumni/Abhishek.png";
+import alumniImg2 from "../../assets/Alumni/Adithya Rngaraju.jpeg";
+import alumniImg3 from "../../assets/Alumni/Alagu Nachiappan.jpg";
+import alumniImg4 from "../../assets/Alumni/Amritha Ragavendiran.jpg";
+import alumniImg5 from "../../assets/Alumni/Aroon Deep.jpeg";
+import alumniImg6 from "../../assets/Alumni/Aruna Sree.jpg";
+
+import alumniImg7 from "../../assets/Alumni/Dr Manigandan.jpg";
+import alumniImg8 from "../../assets/Alumni/Dr.Abhay.jpg";
+import alumniImg9 from "../../assets/Alumni/Dr.Janikaa.jpg";
+import alumniImg10 from "../../assets/Alumni/Dr.Jeneeta Eliza John.jpeg";
+import alumniImg11 from "../../assets/Alumni/Dr.Lakshman Balaji.png";
+import alumniImg12 from "../../assets/Alumni/Dr.Poorani Kanagasabapathi.jpg";
+import alumniImg13 from "../../assets/Alumni/Dr.Prasanthi.jpg";
+
+import alumniImg14 from "../../assets/Alumni/Gayathri Sreedhar.jpeg";
+import alumniImg15 from "../../assets/Alumni/Harshini.jpeg";
+import alumniImg16 from "../../assets/Alumni/Miruthula Asokan.jpeg";
+import alumniImg17 from "../../assets/Alumni/Niranjan Harikrishna.jpg";
+import alumniImg18 from "../../assets/Alumni/Parithima.jpg";
+import alumniImg19 from "../../assets/Alumni/Sudarsana Samhita.png";
+import alumniImg20 from "../../assets/Alumni/Thilak Vikram.jpeg";
+import alumniImg21 from "../../assets/Alumni/Vignesh Prabhu.png";
+import alumniImg22 from "../../assets/Alumni/Vineeth.jpg";
+
+
+
+export const NoteableAlumniData = [
+  {
+    id: 1,
+    name: "Abhishek",
+    currentDesignation: "Tesla Austin",
+    image: alumniImg1,
+  },
+  {
+    id: 2,
+    name: "Adithya Rngaraju",
+    currentDesignation: "Global Development Engineer",
+    image: alumniImg2,
+  },
+  {
+    id: 3,
+    name: "Alagu Nachiappan",
+    currentDesignation: "Business  Essaar Travels",
+    image: alumniImg3,
+  },
+  {
+    id: 4,
+    name: "Amritha Ragavendiran",
+    currentDesignation: "Harvard University",
+    image: alumniImg4,
+  },
+  {
+    id: 5,
+    name: "Aroon Deep",
+    currentDesignation: "Principal Correspondent at The Hindu",
+    image: alumniImg5,
+  },
+
+  {
+    id: 6,
+    name: "Aruna Sree",
+    currentDesignation: "Senior Engineer, L&T Construction (M&M SBG)",
+    image: alumniImg6,
+  },
+  {
+    id: 7,
+    name: "Dr Manigandan",
+    currentDesignation: "",
+    image: alumniImg7,
+  },
+  {
+    id: 8,
+    name: "Dr.Abhay",
+    currentDesignation: "",
+    image: alumniImg8,
+  },
+  {
+    id: 9,
+    name: "Dr.Janikaa",
+    currentDesignation: "",
+    image: alumniImg9,
+  },
+  {
+    id: 10,
+    name: "Dr.Jeneeta Eliza John",
+    currentDesignation: "",
+    image: alumniImg10,
+  },
+  {
+    id: 11,
+    name: "Dr.Lakshman Balaji",
+    currentDesignation: "",
+    image: alumniImg11,
+  },
+  {
+    id: 12,
+    name: "Dr.Poorani Kanagasabapathi",
+    currentDesignation: "",
+    image: alumniImg12,
+  },
+  {
+    id: 13,
+    name: "Dr.Prasanthi",
+    currentDesignation: "",
+    image: alumniImg13,
+  },
+  {
+    id: 14,
+    name: "Gayathri Sreedhar",
+    currentDesignation: "MSc  University of Edinburgh",
+    image: alumniImg14,
+  },
+  {
+    id: 15,
+    name: "Harshini",
+    currentDesignation: "Asst.Manager - The Federal Bank Ltd",
+    image: alumniImg15,
+  },
+  {
+    id: 16,
+    name: "Miruthula Asokan",
+    currentDesignation: "University of Southampton (Masters)",
+    image: alumniImg16,
+  },
+
+  {
+    id: 17,
+    name: "Niranjan Harikrishna",
+    currentDesignation: "Dentist",
+    image: alumniImg17,
+  },
+  {
+    id: 18,
+    name: "Parithima mathanan",
+    currentDesignation: "Mind Coach Psychologist",
+    image: alumniImg18,
+  },
+  {
+    id: 19,
+    name: "Sudarsana Samhita",
+    currentDesignation: "Associate Engineer, Caterpillar, Chennai",
+    image: alumniImg19,
+  },
+  {
+    id: 20,
+    name: "Thilak Vikram",
+    currentDesignation:
+      "Founder and Principal Designer - Vikram Design Studios",
+    image: alumniImg20,
+  },
+  {
+    id: 21,
+    name: "Vignesh Prabhu",
+    currentDesignation: "Senior Consultant at Grant Thornton",
+    image: alumniImg21,
+  },
+  {
+    id: 22,
+    name: "Vineeth",
+    currentDesignation: "Senior Analyst  WNS Global Services",
+    image: alumniImg22,
+  },
+];
